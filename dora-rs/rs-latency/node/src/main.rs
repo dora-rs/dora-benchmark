c89a4f7b3aa1da5e6f653bdb47f084bbf095c3a0
haixuanTao <tao.xavier@outlook.com>
2023-11-22 14:22:51 +0100

    Update source node to use ticker

--- dora-rs/rs-latency/node/src/main.rs
+++ dora-rs/rs-latency/node/src/main.rs
@@ -1,3 +1,4 @@
+use dora_node_api::Event;
 use dora_node_api::{self, arrow::array::UInt64Array, dora_core::config::DataId, DoraNode};
 use rand::Rng;
 use std::time::Duration;
@@ -7,26 +8,36 @@ fn main() -> eyre::Result<()> {
     let latency = DataId::from("latency".to_owned());
     let _throughput = DataId::from("throughput".to_owned());
 
-    let (mut node, _events) = DoraNode::init_from_env()?;
+    let (mut node, mut events) = DoraNode::init_from_env()?;
     let sizes = [1, 10 * 512, 100 * 512, 1000 * 512, 10000 * 512];
 
     // test latency first
     for size in sizes {
         for _ in 0..100 {
-            let mut random_data: Vec<u64> = rand::thread_rng()
-                .sample_iter(rand::distributions::Standard)
-                .take(size)
-                .collect();
-            let t_send = system_time_clock().as_u64();
-            let beginning_slice = random_data.get_mut(0).unwrap();
-            *beginning_slice = t_send;
+            if let Some(event) = events.recv() {
+                match event {
+                    Event::Input {
+                        id: _,
+                        data: _,
+                        metadata,
+                    } => {
+                        let mut random_data: Vec<u64> = rand::thread_rng()
+                            .sample_iter(rand::distributions::Standard)
+                            .take(size)
+                            .collect();
+                        let t_send = system_time_clock().as_u64();
+                        let beginning_slice = random_data.get_mut(0).unwrap();
+                        *beginning_slice = t_send;
 
-            let random_data: UInt64Array = random_data.into();
+                        let random_data: UInt64Array = random_data.into();
 
-            node.send_output(latency.clone(), Default::default(), random_data)?;
-
-            // sleep a bit to avoid queue buildup
-            std::thread::sleep(Duration::from_millis(20));
+                        node.send_output(latency.clone(), metadata.parameters, random_data)?;
+                    }
+                    _ => {}
+                }
+            } else {
+                break;
+            }
         }
     }
 
