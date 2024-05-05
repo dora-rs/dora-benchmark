a867bca9ce3a1ac56aed81107c2a5bca3364899a
haixuanTao <tao.xavier@outlook.com>
2024-05-05 10:43:44 +0200

    Use longer span timer

--- ros2/py_pubsub/py_pubsub/publisher_member_function.py
+++ ros2/py_pubsub/py_pubsub/publisher_member_function.py
@@ -20,11 +20,7 @@ from rclpy.node import Node
 from std_msgs.msg import UInt64MultiArray
 
 SIZES = [
-    8,
-    64,
     512,
-    2048,
-    4 * 512,
     10 * 512,
     100 * 512,
     1000 * 512,
@@ -37,7 +33,7 @@ class MinimalPublisher(Node):
     def __init__(self):
         super().__init__("minimal_publisher")
         self.publisher_ = self.create_publisher(UInt64MultiArray, "topic", 10)
-        timer_period = 0.05  # seconds
+        timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
         self.j = 0
